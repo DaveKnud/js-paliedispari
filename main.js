c89ff9b03949c4303624668c9e7dec3c8b1b6314
DaveKnud <daveknud@outlook.com>
2023-05-08 16:45:27 +0200

    creazione condizione (ex palindromo)

---
+++ main.js
@@ -0,0 +1,16 @@
+const container = document.getElementById("container");
+
+//Chiedere a utente una parola
+const parolaUtente = prompt("inserisci parola polindroma");
+console.log(parolaUtente);
+
+//Reverse parola utente
+let reversed = parolaUtente.split("").reverse().join("");
+console.log(reversed);
+
+//Creare una funzione per capire se la parola inserita è palindroma
+if (parolaUtente)
+
+
+
+
