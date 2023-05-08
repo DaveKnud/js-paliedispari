69ad694a92cd7f260125864ee2a0f695b748abdb
DaveKnud <daveknud@outlook.com>
2023-05-08 16:51:24 +0200

    Ex palindromo completato (?)

--- main.js
+++ main.js
@@ -9,7 +9,14 @@ let reversed = parolaUtente.split("").reverse().join("");
 console.log(reversed);
 
 //Creare una funzione per capire se la parola inserita è palindroma
-if (parolaUtente)
+if (parolaUtente === reversed) {
+    container.innerHTML += `<h1> La tua parola e' palindroma </h1>`;
+    console.log("La tua parola e' palindroma");
+}
+
+else{
+    container.innerHTML += `<h1> La tua parola NON e' palindroma </h1>`;
+}
 
 
 
